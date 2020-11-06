c462f1d4b306cab7774b618316a0f1f225a8a7b2
cvillela <csvillela1@gmail.com>
2020-11-06 12:02:21 -0300

    Update ScaleManager.h modes and notes

--- ScaleManager.h
+++ ScaleManager.h
@@ -2,42 +2,44 @@
 #define ScaleManager_h
 
 //************************************************************************
-//Scale ()
-//class ScaleManager
-//{
-//  public:
-//    ScaleManager();
-//    void updateRoot();
-//    void updateMode();
-//    void updateScale();
-//    char* root_name;
-//    char* mode;
-//    int* note_values;
-//    int octave;
-//  
-//  private:
-//    // Notes and Scales constant values
-//    static const int C = 0;
-//    static const int Cs = 1;
-//    static const int D = 2;
-//    static const int Ds = 3;
-//    static const int E = 4;
-//    static const int F = 5;
-//    static const int Fs = 6;
-//    static const int G = 7;
-//    static const int Gs = 8;
-//    static const int A = 9;
-//    static const int As = 10;
-//    static const int B = 11;
-//
-//    const int* JONIO[]   =   [1, 1, 0, 1, 1, 1, 0];
-//    const int* DORICO[]  =   [1, 0, 1, 1, 1, 0, 1];
-//    const int* FRIGIO[]  =   [0, 1, 1, 1, 0, 1, 1];
-//    const int* LIDIO[]   =   [1, 1, 1, 0, 1, 1, 0];
-//    const int* MIXOLIDIO[] = [1, 1, 0, 1, 1, 0, 1];
-//    const int* EOLIO[]   =   [1, 0, 1, 1, 0, 1, 1];
-//    const int* LOCRIO[]  =   [0, 1, 1, 0, 1, 1, 1];
-//
-//};
+Scale ()
+class ScaleManager
+{
+ public:
+   ScaleManager();
+   void updateRoot();
+   void updateMode();
+   void updateScale();
+   
+   int SMroot;
+   int SMoctave;
+   int SMmode;
+
+   int* SMnotes;
+
+ private:
+   // Notes and Scales constant values
+   const int C = 0;
+   const int Cs = 1;
+   const int D = 2;
+   const int Ds = 3;
+   const int E = 4;
+   const int F = 5;
+   const int Fs = 6;
+   const int G = 7;
+   const int Gs = 8;
+   const int A = 9;
+   const int As = 10;
+   const int B = 11;
+
+   const int JONIO[]   =   [1, 1, 0, 1, 1, 1, 0];
+   const int DORICO[]  =   [1, 0, 1, 1, 1, 0, 1];
+   const int FRIGIO[]  =   [0, 1, 1, 1, 0, 1, 1];
+   const int LIDIO[]   =   [1, 1, 1, 0, 1, 1, 0];
+   const int MIXOLIDIO[] = [1, 1, 0, 1, 1, 0, 1];
+   const int EOLIO[]   =   [1, 0, 1, 1, 0, 1, 1];
+   const int LOCRIO[]  =   [0, 1, 1, 0, 1, 1, 1];
+
+};
 
 #endif
