a4ba2d2f35182dcaef8c4e13e192639a1fbfee3e
a166527 <62819737+a166527@users.noreply.github.com>
2020-12-12 19:22:43 -0300

    Update and rename displayTest_ino.ino to lcdManager.c

--- lcdManager.c
+++ lcdManager.c
@@ -1,17 +1,21 @@
-// MIDI Board project - LCD Tests
-// Lab. de Embarcados ES070 - Angelo H Barbosa 166527 ; Caio S Vilella RA
+/* *************************************************** */
+/* **************** ES070 **************************** */
+/* File name: LCDManager                               */
+/* File description: This file implements the functions*/
+/*              of the 160x160 LCD and is used to test */
+/*           differents layouts for the screen content */
+/*                                                     */
+/* Project: MIDIBoard                                  */
+/* Author: Angelo H Barbosa e Caio S Vilella           */
+/* Date: 12/2020                                       */
+/* *************************************************** */
 
-//pin usage as follow:
-//             CS  CD  RST  MOSI  MISO  CLK  LED  
-//Arduino Uno  A5  A3  A4   A2    NONE  A1   A3
-//Arduino Uno  10  9   8    11    NONE  13   A3
-
+#include <displayTest.h>
 #include <LCDWIKI_GUI.h> //Core graphics library
 #include <LCDWIKI_SPI.h> //Hardware-specific library
 #include <SPI.h>
 
-LCDWIKI_SPI mylcd(SSD1283A,10,9,8,A3); //hardware spi,cs,cd,reset
-
+/* Colors definitions */
 #define  BLACK   0x0000
 #define BLUE    0x001F
 #define RED     0xF800
@@ -21,39 +25,28 @@ LCDWIKI_SPI mylcd(SSD1283A,10,9,8,A3); //hardware spi,cs,cd,reset
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
+/* To manage the LCD with SPI is necessary to intantiate the LCDWIKI_SPI class */
+LCDWIKI_SPI mylcd(SSD1283A,10,9,8,A3); //hardware spi,CS,CD,RESET
 
-/*Cabeçalho da tela*/
-unsigned long show_text(void)
-{
-    unsigned long i;
-    unsigned long time_start = micros();
-    mylcd.Set_Draw_color(128, 128, 128); // CINZA
-    mylcd.Fill_Rectangle(0, 0, mylcd.Get_Display_Width()-1, 10);
-    mylcd.Set_Text_colour(GREEN);   
-    mylcd.Set_Text_Size(1);
-    mylcd.Set_Text_Mode(1);
-    mylcd.Print_String("MIDIBoard Test", CENTER, 1);
-
-    mylcd.Set_Draw_color(128, 128, 128); // CINZA
-    mylcd.Fill_Rectangle(0, mylcd.Get_Display_Height()-1-10, mylcd.Get_Display_Width()-1, mylcd.Get_Display_Height()-1);
-    mylcd.Set_Text_colour(WHITE);   
-    mylcd.Set_Text_Size(1);
-    mylcd.Set_Text_Mode(1);
-    mylcd.Print_String("Angelo e Caio - ES070", CENTER, mylcd.Get_Display_Height()-10);
-
-    mylcd.Set_Draw_color(CYAN); // Cor da borda da tela principal
-    mylcd.Draw_Rectangle(0, 11, mylcd.Get_Display_Width()-1, mylcd.Get_Display_Height()-1-11);   
-    
-    i = show_sinewave();
-    clear_screen();
-    
-    return micros() - time_start;
-}
-
-unsigned long show_sinewave(void)
+/* *************************************************** */
+/* **************** ES070 **************************** */
+/* Method name: showSinewave                           */
+/* Method description: This function implements the    */
+/*                     sinewave used on the opening    */
+/*                     screen                          */
+/* Input params: void                                  */
+/*                                                     */
+/* Output params: unsigned long that represents the    */
+/*                time spent by this routine           */
+/*                                                     */
+/* Project: MIDIBoard                                  */
+/* Author: Angelo H Barbosa e Caio S Vilella           */
+/* Date: 12/2020                                       */
+/* *************************************************** */
+unsigned long showSinewave(void)
 {
    uint16_t buf[mylcd.Get_Display_Width()-2],x = 1,i,y;
-   unsigned long time_start = micros();
+   unsigned long uiTimeStart = micros();
    int16_t t = 20, b = 40;
    float k = 4;
 
@@ -81,13 +74,78 @@ unsigned long show_sinewave(void)
     mylcd.Draw_Pixel(x,y);
     buf[x-1]=y;
   }
-  return micros()- time_start;   
+  return micros()-uiTimeStart;   
 }
 
-unsigned long show_LooperButtons(int x, int y)
+/* *************************************************** */
+/* **************** ES070 **************************** */
+/* Method name: showBegin                              */
+/* Method description: This function implements the    */
+/*                     initialization screen           */
+/* Input params: void                                  */
+/*                                                     */
+/* Output params: unsigned long that represents the    */
+/*                time spent by this opening           */
+/*                                                     */
+/* Project: MIDIBoard                                  */
+/* Author: Angelo H Barbosa e Caio S Vilella           */
+/* Date: 12/2020                                       */
+/* *************************************************** */
+unsigned long showBegin(void)
 {
-    unsigned long time_start = micros();
+    unsigned long uiTimeStart = micros();
     
+    /*Setting the top layout*/
+    mylcd.Set_Draw_color(128, 128, 128); // CINZA
+    mylcd.Fill_Rectangle(0, 0, mylcd.Get_Display_Width()-1, 10);
+    /*Setting the top text*/
+    mylcd.Set_Text_colour(GREEN);   
+    mylcd.Set_Text_Size(1);
+    mylcd.Set_Text_Mode(1);
+    mylcd.Print_String("MIDIBoard Project", CENTER, 1);
+
+    /*Setting the bottom layout*/
+    mylcd.Set_Draw_color(128, 128, 128); // CINZA
+    mylcd.Fill_Rectangle(0, mylcd.Get_Display_Height()-1-10, mylcd.Get_Display_Width()-1, mylcd.Get_Display_Height()-1);
+    /*Setting the bottom text*/
+    mylcd.Set_Text_colour(WHITE);   
+    mylcd.Set_Text_Size(1);
+    mylcd.Set_Text_Mode(1);
+    mylcd.Print_String("Angelo e Caio", CENTER, mylcd.Get_Display_Height()-10);
+
+    /*Setting border color of opening screen*/
+    mylcd.Set_Draw_color(CYAN); // Cor da borda da tela principal
+    mylcd.Draw_Rectangle(0, 11, mylcd.Get_Display_Width()-1, mylcd.Get_Display_Height()-1-11);   
+    
+    /*Setting opening effect*/
+    i = showSinewave();
+    
+    /*Cleaning screen before initiate the main screen*/
+    clear_screen();
+    
+    return micros() - uiTimeStart;
+}
+
+/* *************************************************** */
+/* **************** ES070 **************************** */
+/* Method name: showLooperButtons                      */
+/* Method description: This function implements the    */
+/*                     Looper buttons on screen        */
+/* Input params: int x: x coordinate reference         */
+/*               int y: y coordinate reference         */
+/*                                                     */
+/* Output params: unsigned long that represents the    */
+/*                time spent by this function          */
+/*                                                     */
+/* Project: MIDIBoard                                  */
+/* Author: Angelo H Barbosa e Caio S Vilella           */
+/* Date: 12/2020                                       */
+/* *************************************************** */
+unsigned long showLooperButtons(int x, int y)
+{
+    unsigned long uiTimeStart = micros();
+    
+    /*Setting L1 button layout*/
     mylcd.Set_Draw_color(GREEN);
     mylcd.Fill_Round_Rectangle(x+10, y+15, x+33, y+38, 5);
     mylcd.Set_Text_colour(WHITE);   
@@ -95,6 +153,7 @@ unsigned long show_LooperButtons(int x, int y)
     mylcd.Set_Text_Mode(1);
     mylcd.Print_String("L1", x+12, y+20);
     
+    /*Setting L2 button layout*/
     mylcd.Set_Draw_color(GREEN);
     mylcd.Fill_Round_Rectangle(x+38, y+15, x+61, y+38, 5);
     mylcd.Set_Text_colour(WHITE);   
@@ -102,6 +161,7 @@ unsigned long show_LooperButtons(int x, int y)
     mylcd.Set_Text_Mode(1);
     mylcd.Print_String("L2", x+39, y+20);
     
+    /*Setting L3 button layout*/
     mylcd.Set_Draw_color(GREEN);
     mylcd.Fill_Round_Rectangle(x+66, y+15, x+89, y+38, 5);
     mylcd.Set_Text_colour(WHITE);   
@@ -109,6 +169,7 @@ unsigned long show_LooperButtons(int x, int y)
     mylcd.Set_Text_Mode(1);
     mylcd.Print_String("L3", x+67, y+20);
     
+    /*Setting L4 button layout*/
     mylcd.Set_Draw_color(GREEN);
     mylcd.Fill_Round_Rectangle(x+94, y+15, x+117, y+38, 5);
     mylcd.Set_Text_colour(WHITE);   
@@ -116,14 +177,28 @@ unsigned long show_LooperButtons(int x, int y)
     mylcd.Set_Text_Mode(1);
     mylcd.Print_String("L4", x+95, y+20);
      
-     return micros()- time_start;   
+    return micros()- uiTimeStart;   
 }
 
-//void Fill_Triangle(int16_t x0, int16_t y0, int16_t x1, int16_t y1,int16_t x2, int16_t y2)
-
-unsigned long show_RSOButtons(int x, int y, String R, String S, String O)
+/* *************************************************** */
+/* **************** ES070 **************************** */
+/* Method name: showFeatureButtons                     */
+/* Method description: This function implements the    */
+/*                     different features buttons      */
+/* Input params: int x: x coordinate reference         */
+/*               int y: y coordinate reference         */
+/*               String R, S, O: Values of the features*/
+/*                                                     */
+/* Output params: unsigned long that represents the    */
+/*                time spent by this function          */
+/*                                                     */
+/* Project: MIDIBoard                                  */
+/* Author: Angelo H Barbosa e Caio S Vilella           */
+/* Date: 12/2020                                       */
+/* *************************************************** */
+unsigned long showFeatureButtons(int x, int y, String R, String S, String O)
 {
-    unsigned long time_start = micros();
+    unsigned long uiTimeStart = micros();
     
     mylcd.Set_Draw_color(YELLOW);
     mylcd.Fill_Triangle(x,y,x+10,y-10,x+10,y+10);
@@ -164,7 +239,7 @@ unsigned long show_RSOButtons(int x, int y, String R, String S, String O)
     mylcd.Set_Text_Mode(1);
     mylcd.Print_String("Octave", x+60, y+42);
     
-    return time_start;
+    return micros() - uiTimeStart;
 }
 
 
